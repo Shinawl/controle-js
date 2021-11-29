2fec65c98b1c0c2d3724d483797c038fa395c9ac
tom.perret51@gmail.com <tom.perret51@gmail.com>
2021-11-29 15:58:34 +0100

    modif

--- src/components/TimeInput.vue
+++ src/components/TimeInput.vue
@@ -2,19 +2,63 @@
   <div class="hello">
     <p>Choose one of the available timeslots</p>
 
-    <div>
-      <input type="radio" id="huey" name="drone" value="huey">
-      <label for="huey">Huey</label>
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">12:00</label>
+    </div>
+    
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">12:30</label>
     </div>
 
-    <div>
-      <input type="radio" id="dewey" name="drone" value="dewey">
-      <label for="dewey">Dewey</label>
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">13:00</label>
     </div>
 
-      <div>
-      <input type="radio" id="louie" name="drone" value="louie">
-      <label for="louie">Louie</label>
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">13:30</label>
+    </div>
+
+    <div class="radio-toolbar prime">
+      <input type="radio" id="prime" name="hour" value="prime">
+      <label for="prime">14:00</label>
+    </div>
+    <div class="radio-toolbar prime">
+      <input type="radio" id="prime" name="hour" value="prime">
+      <label for="prime">14:30</label>
+    </div>
+    <div class="radio-toolbar prime">
+      <input type="radio" id="prime" name="hour" value="prime">
+      <label for="prime">15:00</label>
+    </div>
+
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">15:30</label>
+    </div>
+    <div class="radio-toolbar regular">
+      <input type="radio" id="regular" name="hour" value="regular">
+      <label for="regular">16:00</label>
+    </div>
+
+    <div class="radio-toolbar premium">
+      <input type="radio" id="premium" name="hour" value="premium">
+      <label for="premium">18:00</label>
+    </div>
+    <div class="radio-toolbar premium">
+      <input type="radio" id="premium" name="hour" value="premium">
+      <label for="premium">18:30</label>
+    </div>
+    <div class="radio-toolbar premium">
+      <input type="radio" id="premium" name="hour" value="premium">
+      <label for="premium">19:00</label>
+    </div>
+    <div class="radio-toolbar premium">
+      <input type="radio" id="premium" name="hour" value="premium">
+      <label for="premium">18:30</label>
     </div>
 
   </div>
@@ -31,5 +75,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .radio-toolbar input[type="radio"] {
+  opacity: 0;
+  position: fixed;
+  width: 0;
+  }
+
+  .radio-toolbar label {
+    display: inline-block;
+    background-color: #ddd;
+    padding: 10px 20px;
+    font-family: sans-serif, Arial;
+    font-size: 16px;
+    border: 2px solid #444;
+    border-radius: 4px;
+  }
+
+  .radio-toolbar input[type="radio"]:checked + label {
+    background-color:#bfb;
+    border-color: #4c4;
+  }
+
 
 </style>
